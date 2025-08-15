e127310ff6f96e62044b6cf474b14a99e2a4dcf2
정선우 <swjung@pentasecurity.com>
2025-08-15 19:15:49 +0900

    add: spring setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "org.example"
+group = "org.fastcampus"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,8 +12,23 @@ repositories {
 }
 
 dependencies {
+    //spring
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    //mysql
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.5.3")
+
+    //lombok
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    //test
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
